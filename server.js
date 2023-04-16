d0e55a9ff72ee40481c5837779fd02906323ce40
RoyAbra27 <97115783+RoyAbra27@users.noreply.github.com>
2023-04-16 15:55:15 +0300

    Added server config

--- server.js
+++ server.js
@@ -33,3 +33,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.error('SIGTERM RECIEVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
